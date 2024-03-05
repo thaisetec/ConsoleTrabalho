d6574eada4ade8e570e833200ac691da3fa9c72c
Usuario <Usuario@CPD02-M10>
2024-03-05 11:29:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleTrabalho/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTrabalho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Qual é o salário bruto?");
+            double sal = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor do seu vale alimentação");
+            double val = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos filhos vc tem?");
+            int fil = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual valor a empresa disponibiliza por filho?");
+            int valorfil = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas horas extras vc fez?");
+           int qnthora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual valor por hora?");
+            double porhora = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite o valor do inss");
+         double inss = double.Parse (Console.ReadLine());
+            Console.WriteLine("digite o valor do irf");
+            double irf =double.Parse(Console.ReadLine());
+            double liquido = sal = val + (fil * valorfil) +
+               (qnthora * porhora) - inss - irf;
+            Console.WriteLine("salario liquido: " + liquido.ToString());
+            Console.ReadKey();
+        }
+    }
+}
